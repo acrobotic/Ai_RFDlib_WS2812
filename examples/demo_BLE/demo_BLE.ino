67b1daf1742719e373d57ca277aa07647a92672c
1sc0bot <x1sc0@acrobotic.com>
2015-10-20 08:30:06 -0700

    updating

--- examples/demo_BLE/demo_BLE.ino
+++ examples/demo_BLE/demo_BLE.ino
@@ -1,53 +1,12 @@
-/*
-The sketch receives a Bluetooth Low Energy 4 connection,
-then uses PWM on the RGB shield to approximate the color
-selected.
-
-This sketch is supposed to be used with the rfduinoColorWheel
-iPhone application.
-*/
-
-/*
- Copyright (c) 2014 OpenSourceRF.com.  All right reserved.
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
-
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- See the GNU Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 #include <RFduinoBLE.h>
 #include <Ai_RFD_WS2812.h>
 
 #define PIN     2
-#define NUM     24
+#define NUM_LED 1
 
-RFD_WS2812 blinker = RFD_WS2812(NUM, PIN);
-
+RFD_WS2812 blinker = RFD_WS2812(NUM_LED, PIN);
 
 void setup() {
-  // setup the leds for output
-  // Initialize WS2812 modules with all LEDs turned off
-  blinker.initialize();
-  blinker.setBrightness(100); 
-
   // this is the data we want to appear in the advertisement
   // (if the deviceName and advertisementData are too long to fix into the 31 byte
   // ble advertisement packet, then the advertisementData is truncated first down to
@@ -56,25 +15,38 @@ void setup() {
   
   // start the BLE stack
   RFduinoBLE.begin();
+
+  // Initialize WS2812 modules with all LEDs turned off
+  blinker.initialize();
+
+  // Because of the high brightness of the LEDs, we might want to reduce it.
+  // The range is between 0 to 255.
+  // blinker.setBrightness(100); 
+
+  // For debugging purposes use the Serial object.
+  Serial.begin(9600);
 }
 
 void loop() {
-  // switch to lower power mode
+  // Switch to lower power mode
   RFduino_ULPDelay(INFINITE);
 }
 
 void RFduinoBLE_onConnect() {
-  // the default starting color on the iPhone is white
-  blinker.clear();
+  // Debug message printed to Serial interface
+  Serial.println("RFduino connected");
 }
 
 void RFduinoBLE_onDisconnect() {
-  // the default starting color on the iPhone is white
-  blinker.clear();
+  // Debug message printed to Serial interface
+  Serial.println("RFduino disconnected");
 }
 
 void RFduinoBLE_onReceive(char *data, int len) {
-  // each transmission should contain an RGB triple
+  // Debug message printed to Serial interface
+  Serial.print("Data received: ");
+  Serial.println(data);
+  // Each transmission should contain an RGB triple
   if (len >= 3)
   {
     // get the RGB values
@@ -82,10 +54,8 @@ void RFduinoBLE_onReceive(char *data, int len) {
     uint8_t g = data[1];
     uint8_t b = data[2];
     colorWipe(blinker.packRGB(r,g,b),25);
-
   }
 }
-
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) 
